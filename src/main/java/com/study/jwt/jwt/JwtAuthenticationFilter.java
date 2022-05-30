746bc7eef70e4dfc7a2ad57409317ab2b37cfe16
HanGyulKang <gang6607@gmail.com>
2022-05-31 01:38:10 +0900

    2022 05 31

--- src/main/java/com/study/jwt/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/study/jwt/jwt/JwtAuthenticationFilter.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
@@ -61,7 +63,9 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
             // 로그인 시도
             // PrincipalDetailsService의 loadUserByUsername() 함수가 실행 됨
+            // 함수 실행 이후 정상이면 authentication이 return 됨
             // password는 스프링이 처리 함... ㅠㅠㅠㅠㅠ username만 있으면 됨
+            // DB에 있는 username과 password 일치 여부를 판단.
             Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
             // 데이터 확인
@@ -71,7 +75,10 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             System.out.println("principalDetails.getUser().getPassword() : " + principalDetails.getUser().getPassword());
 
             System.out.println("======================= 로그인 성공 =======================");
+
             // return 될 때 authentication 객체가 session 영역에 저장 됨
+            // 리턴의 이유는 권한 관리를 security가 대신 해주기 때문에 편하려고 하는거임
+            // 굳이 JWT 토큰을 사용하면서 session을 만들 이유가 없으나 단지 권한처리때문에 session에 넣어주는거임
             return authentication;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -83,5 +90,15 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         // 5. PrincipalDetails를 세션에 담는다. (세션에 안 담으면 권한 관리가 안 됨 : 권한관리 안 할거면 안 담아도 됨)
         // 6. JWT 토큰을 만들어서 응답해주면 됨
         // return super.attemptAuthentication(request, response);
+    }
+
+    /**
+     * attemptAuthentication() 실행 이후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행 됨.
+     * JWT 토큰을 만들어서 request 요청한 사용자에게 JWT 토큰을 reponse 해주면 됨
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("successfulAuthentication 실행 : 인증 완료");
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
